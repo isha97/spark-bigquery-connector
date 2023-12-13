df1a5f87cff97991311567fa813f21e6f356d232
Isha Tarte <tarteisha97@gmail.com>
2023-12-12 16:04:36 -0800

    fix scala 2.12

--- spark-bigquery-dsv1/spark-bigquery-with-dependencies_2.12/src/test_2.12/java/com/google/cloud/spark/bigquery/acceptance/Scala212BigNumericDataprocServerlessAcceptanceTest.java
+++ spark-bigquery-dsv1/spark-bigquery-with-dependencies_2.12/src/test_2.12/java/com/google/cloud/spark/bigquery/acceptance/Scala212BigNumericDataprocServerlessAcceptanceTest.java
@@ -18,7 +18,7 @@ package com.google.cloud.spark.bigquery.acceptance;
 public class Scala212BigNumericDataprocServerlessAcceptanceTest extends BigNumericDataprocServerlessAcceptanceTestBase {
 
     public Scala212BigNumericDataprocServerlessAcceptanceTest() {
-        super("spark-bigquery", "1.0");
+        super("spark-bigquery", "1.0", "spark-bigquery-with-dependencies_2.12-0.34.0");
     }
 
     // tests from superclass
